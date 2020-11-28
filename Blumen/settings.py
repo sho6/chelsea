7fb9fbc4d98e9a6b1a9c95fdb154bb1a52b818b1
shoshosho <tenpa10.kurukuru@gmail.com>
2020-11-28 12:25:49 +0900

    setting on DB for MySQL

--- Blumen/settings.py
+++ Blumen/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flower_image_serch.apps.FlowerImageSerchConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blumen',
-        'USER': 'root'
+        'USER': 'blumen',
+        'PASSWORD': 'Blumen',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
 
