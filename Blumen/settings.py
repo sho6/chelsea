d7985b5e6e67de8205849a39be6157877e4edc14
shoshosho <tenpa10.kurukuru@gmail.com>
2020-12-05 11:50:48 +0900

    make migrations by django

--- Blumen/settings.py
+++ Blumen/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'flower_image_serch.apps.FlowerImageSerchConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'flower_image_serch.apps.FlowerImageSerchConfig'
 ]
 
 MIDDLEWARE = [
--- flower_image_serch/models.py
+++ flower_image_serch/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Colors(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+
+class Flowers(models.Model):
+    name = models.CharField(max_length=100)
+    petal_number = models.IntegerField()
+    color = models.ForeignKey(Colors, on_delete=models.SET_NULL, blank=True, null=True)
+
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    old = models.IntegerField()
+    email = models.EmailField()
+
+class Locations(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+class Favorite(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='favorite_user')
+    flower = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='favorite_flower')
+
+class FindLog(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='finder_user')
+    flower = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='finder_flower')
